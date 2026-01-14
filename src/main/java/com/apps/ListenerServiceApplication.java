7c18b213906bfd88526a92d00c3c273cf3510e44
arunt2005 <arunt2005@gmail.com>
2026-01-15 03:31:36 +0530

    first commit

---
+++ src/main/java/com/apps/ListenerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenerServiceApplication.class, args);
+	}
+
+}
