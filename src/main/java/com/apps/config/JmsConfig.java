635af696801d1ab9a3255490ef054e04a3b3b90c
arunt2005 <arunt2005@gmail.com>
2026-01-15 06:04:10 +0530

    JSON message support.

--- src/main/java/com/apps/config/JmsConfig.java
+++ src/main/java/com/apps/config/JmsConfig.java
@@ -1,9 +1,24 @@
 package com.apps.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 @EnableJms
 public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        // Use JacksonJsonMessageConverter instead of MappingJackson2MessageConverter
+        JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
 }
--- src/main/java/com/apps/listener/ActiveMqMessageListener.java
+++ src/main/java/com/apps/listener/ActiveMqMessageListener.java
@@ -1,5 +1,6 @@
 package com.apps.listener;
 
+import com.apps.dto.OrderMessage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,12 @@ public class ActiveMqMessageListener {
     @JmsListener(destination = "${messaging.queue}")
     public void onMessage(String message) {
         System.out.println("📩 ActiveMQ received message: " + message);
+    }
+
+    @JmsListener(destination = "order-queue")
+    public void onMessage(OrderMessage order) {
+        // The JSON is already converted back to an OrderRequest object here
+        System.out.println("Received order: " + order.getProduct() + " x" + order.getQuantity());
     }
 }
 
